64e23136adb9cf4725cf74d1a1f5656697b207b7
Adrijan Vranjkovic <adrijan.vranjkovic@ocsbstudent.ca>
2020-03-04 14:30:16 -0500

    Animating Images

--- AnimatingImagesAdrijan/main.lua
+++ AnimatingImagesAdrijan/main.lua
@@ -1,7 +1,16 @@
 -----------------------------------------------------------------------------------------
---
--- main.lua
---
+--Name: Adrijan
+--Class: ICS20
+--This program has a backgroung, displays 3 images that all move, and some colored text.
 -----------------------------------------------------------------------------------------
 
--- Your code here
+--Hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+--Set the background
+local backgroundImage = display.newImageRect( "Images/FamilyGuy.png", 1024, 1000)
+backgroundImage.x = 512
+backgroundImage.y = 359
+
+local Peter = display.newImageRect("Images/PeterPimp.png",200, 400)
+Peter.x = 
