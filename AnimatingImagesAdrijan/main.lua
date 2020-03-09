8fb52f6757814c24c32681ec30c0063c0c3a3ae9
Adrijan Vranjkovic <adrijan.vranjkovic@ocsbstudent.ca>
2020-03-09 14:30:38 -0400

    Updated Assignment #4

--- AnimatingImagesAdrijan/main.lua
+++ AnimatingImagesAdrijan/main.lua
@@ -7,6 +7,10 @@
 --Hide the status bar
 display.setStatusBar(display.HiddenStatusBar)
 
+--------------------------------------------------------------
+-- LOCAL VARIABLES
+-------------------------------------------------------------
+
 --Set the background
 local backgroundImage = display.newImageRect( "Images/FamilyGuy.png", 1024, 1000)
 backgroundImage.x = 512
@@ -33,17 +37,32 @@ Lazer.alpha = 1
 Lazer.strokeWidth = 12
 Lazer:setStrokeColor(255/255, 0/255, 0/255)
 
-local textSize = 40
+local textSize = 45
 
 --Create text
-local zapText = display.newText("Mike finally found Spongebob and Peter, he tried to  \n zap him with his lazer but they just got away", 0, 0, Arial, textSize)
-zapText.x = 500
+local zapText = display.newText("Mike finally found Spongebob and "
+	.. "Peter, he tried to  \n zap him with his lazer but they just"
+	.. " got away", 0, 0, Arial, textSize)
+zapText.x = 525
 zapText.y = 50
 zapText:setFillColor(255/255, 0/255, 0/255)
 zapText.alpha = 1
 
 --global variable
 scrollSpeed = -10
+
+--adding the next background
+local barBackground = display.newImageRect("Images/Bar.png", 1024, 780)
+barBackground.isVisible = false
+barBackground.x = 512
+barBackground.y = 390
+
+--setting peterScrollSpeed
+local peterScrollSpeed = 2
+
+--------------------------------------------------------------
+-- LOCAL FUNCTIONS
+-------------------------------------------------------------
 
 -- Function: MoveSponge
 --Input: this function accepts an event listener
@@ -53,41 +72,48 @@ local function Zap(event)
     -- change the transparentcy of the lazer so it zaps Spongebob
 	Lazer.alpha = Lazer.alpha - 0.1
 	--display the writing
-	zapText.alpha = zapText.alpha - 0.000001
+	zapText.alpha = zapText.alpha - 0.00001
 	
 end
 
+local function MoveSponge (event)
+    -- add the scroll speed to the x-value of the ship
+	Spongebob.y = Spongebob.y + scrollSpeed	
+	Spongebob:rotate( 20 )
+end
+
+local function MovePeter(event)
+	Peter.x = Peter.x + peterScrollSpeed
+	Peter.y = Peter.y + (peterScrollSpeed - 3)
+	Peter.alpha = Peter.alpha - 0.001
+end
+
+local function MovePeterDelay()
+	--MovePeter will be called over and over again
+	Runtime:removeEventListener("enterFrame", Zap)
+	Runtime:removeEventListener("enterFrame", MoveSponge)
+	Runtime:addEventListener("enterFrame", MovePeter)
+end
+
+
+
+local function changeBackgroundDelay()
+	--MovePeter will be called over and over again
+	Runtime:removeEventListener("enterFrame", MovePeter)
+	backgroundImage.isVisible = false
+	barBackground.isVisible = true
+end
+
+--------------------------------------------------------------
+-- FUNCTION CALLS
+-------------------------------------------------------------
 --Zap will be called over and over again
 Runtime:addEventListener("enterFrame", Zap)
-
-
-
-local function MoveSponge (event)
-      -- add the scroll speed to the x-value of the ship
-	    Spongebob.y = Spongebob.y + scrollSpeed	
-	   Spongebob:rotate( 20 )
-end
 
 --MoveSponge will be called over and over again
 Runtime:addEventListener("enterFrame", MoveSponge)
 
---adding the next background
-local barBackground = display.newImageRect("Images/Bar.png", 1024, 768)
-barBackground.alpha = -1
-barBackground.x = 512
-barBackground.y = 359
-
---setting peterScrollSpeed
-local peterScrollSpeed = 2
-local function MovePeter(event)
-		Peter.x = Peter.x + peterScrollSpeed
-	   Peter.y = Peter.y + (peterScrollSpeed - 3)
-	  Peter.alpha = Peter.alpha - 0.001
-	
-      
-end
-
---MovePeter will be called over and over again
-Runtime:addEventListener("enterFrame", MovePeter)
+timer.performWithDelay(1000, MovePeterDelay)
 
 
+timer.performWithDelay(4000, changeBackgroundDelay)
